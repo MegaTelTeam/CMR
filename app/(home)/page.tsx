6f80a02ae3d9c4850cf00d185e5daccc314f4346
AmineGuitouni <Notam1ne2002@gmail.com>
2024-06-10 19:48:52 +0100

    add database client

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,19 @@
+import userModel from "@/lib/database/models/user.model";
+import { connectToDatabase } from "@/lib/database/mongodb";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  await connectToDatabase();
+  
+  const user = new userModel({
+    username: "test",
+    email: "test",
+    hashedPassword: "test",
+  })
+
+  const obj = await user.save()
+  console.log(obj)
+
   return (
     <></>
   );
