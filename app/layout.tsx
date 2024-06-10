aec91081b22109347be0680d2df3e2fe9aa1e5b0
OumaimaFdhila <oumaimafdhila16@gmail.com>
2024-06-10 19:35:25 +0100

    sabena libraries

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NextUIProv from "../providers/nextProvider";
+import {AOSProvider} from "../providers/AOSProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextUIProv>
-          {children}
+          <AOSProvider>
+            {children}
+          </AOSProvider>          
         </NextUIProv>
       </body>
     </html>
