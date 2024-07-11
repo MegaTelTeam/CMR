34aa5473cb86387f02b5334204115de175d56aa7
AmineGuitouni <Notam1ne2002@gmail.com>
2024-07-11 23:45:21 +0100

    fix 2px fel navbar XD

--- components/navbar/navToolTip.tsx
+++ components/navbar/navToolTip.tsx
@@ -4,7 +4,7 @@ import { FaAngleDown } from "react-icons/fa";
 export default function NavToolTip({list,text}:{list:{text:string,link:string,content?:string[]}[],text:string}) {
 
     return (
-        <div className="relative group hover:h-full flex items-center  flex-shrink-0">
+        <div className="relative group hover:h-full flex items-center flex-shrink-0">
 
             <h3 className="cursor-pointer group text-xs 2xl:text-sm text-forground hover:text-cmr-gray-500 transition-all ease-out duration-200 ">
                 {text}
@@ -12,40 +12,42 @@ export default function NavToolTip({list,text}:{list:{text:string,link:string,co
             <span className="block opacity-0 group-hover:opacity-100 transition-all ease-out duration-200 h-[1px] bg-cmr-red-500"></span>
             </h3>
 
-            <div className="absolute opacity-0 z-10 hidden group-hover:opacity-100  group-hover:flex flex-col gap-[2rem] shadow-md py-6 xl:py-8 border-1 bg-white top-[calc(100%+2px)] ">
-                {list.map((item,index)=>{
-                    if (!item.content)
-                        return (
-                        <Link key={`${item.text}-${index}`} className="group/item px-6 xl:px-8  w-fit text-xs 2xl:text-sm text-forground hover:text-cmr-gray-500 transition-all ease-out duration-200 " href={item.link}>
-                            {item.text}
-                            <span className="block w-full opacity-0 group-hover/item:opacity-100 transition-all ease-out duration-200 h-[1px] bg-cmr-red-500"></span>
-                        </Link>
-                    )
-                    return(
-                        <div key={`${item.text}-${index}`} className="group/content ">
-                            <h3 className="cursor-pointer w-fit group/content px-6 xl:px-8 text-xs 2xl:text-sm text-forground hover:text-cmr-gray-500 transition-all ease-out duration-200 ">
+            <div className="absolute top-full pt-[2px]">
+                <div className="opacity-0 z-10 hidden group-hover:opacity-100  group-hover:flex flex-col gap-[2rem] shadow-md py-6 xl:py-8 border-1 bg-white ">
+                    {list.map((item,index)=>{
+                        if (!item.content)
+                            return (
+                            <Link key={`${item.text}-${index}`} className="group/item px-6 xl:px-8  w-fit text-xs 2xl:text-sm text-forground hover:text-cmr-gray-500 transition-all ease-out duration-200 " href={item.link}>
                                 {item.text}
-                                <FaAngleDown className="inline-block ml-1 group-hover/content:rotate-180 transition-all ease-out duration-200"/>
-                            <span className="block opacity-0 group-hover/content:opacity-100 transition-all ease-out duration-200 h-[1px] bg-cmr-red-500"></span>
-                            </h3>
-                            <div className="absolute left-[calc(100%+1px)] w-[400px]  opacity-0 z-10 hidden group-hover/content:opacity-100  group-hover/content:flex flex-col gap-[2rem] shadow-md p-6 xl:p-8 border-1 bg-white top-[calc(100%-70px)]">
-                            {
-                                item.content.map((itm,ind)=>{
-                                    return(
-                                        <Link key={`${item.text}-${ind}`} className="group/item  w-fit text-xs 2xl:text-sm text-forground hover:text-cmr-gray-500 transition-all ease-out duration-200 " href={item.link}>
-                                        {itm}
-                                        <span className="block w-full opacity-0 group-hover/item:opacity-100 transition-all ease-out duration-200 h-[1px] bg-cmr-red-500"></span>
-                                    </Link>
-                                    )
-                                })
-                            }
+                                <span className="block w-full opacity-0 group-hover/item:opacity-100 transition-all ease-out duration-200 h-[1px] bg-cmr-red-500"></span>
+                            </Link>
+                        )
+                        return(
+                            <div key={`${item.text}-${index}`} className="group/content ">
+                                <h3 className="cursor-pointer w-fit group/content px-6 xl:px-8 text-xs 2xl:text-sm text-forground hover:text-cmr-gray-500 transition-all ease-out duration-200 ">
+                                    {item.text}
+                                    <FaAngleDown className="inline-block ml-1 group-hover/content:rotate-180 transition-all ease-out duration-200"/>
+                                <span className="block opacity-0 group-hover/content:opacity-100 transition-all ease-out duration-200 h-[1px] bg-cmr-red-500"></span>
+                                </h3>
+                                <div className="absolute left-[calc(100%+1px)] w-[400px]  opacity-0 z-10 hidden group-hover/content:opacity-100  group-hover/content:flex flex-col gap-[2rem] shadow-md p-6 xl:p-8 border-1 bg-white top-[calc(100%-70px)]">
+                                {
+                                    item.content.map((itm,ind)=>{
+                                        return(
+                                            <Link key={`${item.text}-${ind}`} className="group/item  w-fit text-xs 2xl:text-sm text-forground hover:text-cmr-gray-500 transition-all ease-out duration-200 " href={item.link}>
+                                            {itm}
+                                            <span className="block w-full opacity-0 group-hover/item:opacity-100 transition-all ease-out duration-200 h-[1px] bg-cmr-red-500"></span>
+                                        </Link>
+                                        )
+                                    })
+                                }
+                                </div>
                             </div>
-                        </div>
-                    )
+                        )
+                        
+                    })}
+                        
                     
-                })}
-                    
-                
+                </div>
             </div>
 
         </div>
