122ad65cab84adfc55052fa245b71136f67fe5c1
AmineGuitouni <Notam1ne2002@gmail.com>
2024-07-11 22:28:10 +0100

    Mitgliedschaften page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "cmr-gradient": "linear-gradient(90deg, #000000 0%, #575757 43%, #808080 48.62%, #941530 63.68%)",
       },
       colors: {
         "cmr-red" : {
