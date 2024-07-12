e3a0f41169eccfdca36e25ad10ee81285125be7a
OumaimaFdhila <oumaimafdhila16@gmail.com>
2024-07-12 15:13:43 +0100

    fix hover el footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <div className="w-full h-[120px] flex justify-center items-center">
-            <p className="text-center text-md text-foreground lg:text-white lg:hover:text-foreground transition-all ease-out duration-200">Copyright: © C.M.R. AG, 2024</p>
+        <div className="group w-full h-[120px] flex justify-center items-center">
+            <p className="text-center text-md text-foreground lg:text-white lg:group-hover:text-foreground transition-all ease-out duration-200">Copyright: © C.M.R. AG, 2024</p>
         </div>
     )
 }
