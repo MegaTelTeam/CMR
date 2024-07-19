ff882ebab35ef136c3d3e10738c187e949a1bd4f
AmineGuitouni <Notam1ne2002@gmail.com>
2024-07-19 16:34:59 +0100

    change title style

--- app/[locale]/Referenzen/Printmedien/page.tsx
+++ app/[locale]/Referenzen/Printmedien/page.tsx
@@ -13,7 +13,7 @@ export default function Printmedien(){
     return (
         <div className="container mx-auto min-h-[calc(100vh-124px)] flex flex-col items-center">
             <h1 className="text-center col-span-3 lg:col-span-12 mt-5 mb-14">
-                <span className="text-4xl font-extrabold bg-gradient-to-r from-black via-cmr-gray-500 to-cmr-red-500 bg-clip-text text-transparent">
+                <span className="text-4xl font-extrabold bg-gradient-to-r from-cmr-gray-500 to-cmr-red-500 bg-clip-text text-transparent">
                     Printmedien
                 </span>
             </h1>
